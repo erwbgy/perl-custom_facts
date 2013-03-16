b6bbb2d54e75e58e83092c47aa9edba9beed89bb
Keith Burdis <keith@burdis.org>
2013-03-16 05:23:21 +0000

    Custom fact query

--- lib/custom_facts.pm
+++ lib/custom_facts.pm
@@ -1,10 +1,20 @@
 package custom_facts;
 use Dancer ':syntax';
+use YAML;
+use warnings;
+use strict;
 
 our $VERSION = '0.1';
 
-get '/' => sub {
-    template 'index';
+set serializer => 'JSON';
+
+get '/custom_facts/:host' => sub {
+  my $filename = sprintf( "/etc/puppet/hieradata/hosts/%s.yaml", params->{host} );
+  if (not -r $filename) {
+    send_error( sprintf( "No configuration file found for host '%s'", params->{host} ) );
+  }
+  my $host_config = YAML::LoadFile($filename);
+  return $host_config->{'system::facts'};
 };
 
 true;
