19feb8dd28dd3d9601be34db26d2dd94ad0b12b1
Keith Burdis <keith@burdis.org>
2013-03-16 06:28:03 +0000

    Update custom_facts using RESTful POST request.  Return updated custom facts as a response.  Input and output is JSON.

--- lib/custom_facts.pm
+++ lib/custom_facts.pm
@@ -17,4 +17,19 @@ get '/custom_facts/:host' => sub {
   return $host_config->{'system::facts'};
 };
 
+post '/custom_facts/:host' => sub {
+  my $filename = sprintf( "/etc/puppet/hieradata/hosts/%s.yaml", params->{host} );
+  if (not -r $filename) {
+    send_error( sprintf( "No configuration file found for host '%s'", params->{host} ) );
+  }
+  my $host_config = YAML::LoadFile($filename);
+  info YAML::Dump($host_config);
+  foreach my $fact (keys %{ params('body') }) {
+    info sprintf("%s='%s'", $fact, params->{$fact});
+    $host_config->{'system::facts'}{$fact} = params->{$fact};
+  }
+  YAML::DumpFile($filename, $host_config);
+  return $host_config;
+};
+
 true;
